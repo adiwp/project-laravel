e4df26b3f9c6c3a1c3de23924eabc92ba04f2f9c
Adi Wahyu Pribadi <adi.wahyu.p@gmail.com>
2025-10-03 09:15:39 +0700

    Belajar View

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tentang-kami', function () {
+    return view('about'); // Kita akan menampilkan view bernama 'about'
+});
